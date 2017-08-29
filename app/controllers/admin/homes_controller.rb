48b3826b11f86787658e8aa0b39cdc955eb29035
Koogle.Zhang <koogle.zhang@gmail.com>
2017-08-29 16:37:25 +0800

    添加重启方法判断

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -7,8 +7,9 @@ class Admin::HomesController < Admin::BaseController
 
   def start_service
     List.generate_config
-    o, s = Open3.capture2("ssserver -c #{ShadowSock.first.local_path} -d start")
-    redirect_to admin_root_path, notice:"ShadowSocks Service Starting #{o}, #{s}"
+    o, s = Open3.capture2("ssserver -c #{ShadowSock.first.local_path} -d restart")
+    o, s = Open3.capture2("ssserver -c #{ShadowSock.first.local_path} -d start") if o.blank?
+    redirect_to admin_root_path, notice:"ShadowSocks Service Starting"
   end
 
 private
